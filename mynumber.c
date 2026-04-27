49d33c7755aa61077909e26d21cc28787d3f5bd7
sinjong4182-cmyk <sinjong4182@gmail.com>
2026-04-28 05:07:15 +0900

    prime numbers in Testing branch

--- mynumber.c
+++ mynumber.c
@@ -13,6 +13,18 @@ int main() {
             printf("%d ", i);
         }
     }
+    printf("\n\n");
+   
+    for (int i = 2; i <= 100; i++) {
+        bool is_prime = true;
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                is_prime = false;
+                break;
+            }
+        }
+        if (is_prime) printf("%d ", i);
+    }
     printf("\n");
 
     return 0;
