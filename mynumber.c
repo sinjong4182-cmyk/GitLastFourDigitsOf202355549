e757f275e5c8283f2e44642f62953289fded43b5
sinjong4182-cmyk <sinjong4182@gmail.com>
2026-04-28 04:54:40 +0900

    Update mynumber.c : print odd and even

--- mynumber.c
+++ mynumber.c
@@ -6,6 +6,15 @@ int main() {
             printf("%d ", i);
         }
     }
+    printf("\n\n");
+
+    printf("Even numbers:\n");
+    for (int i = 1; i <= 100; i++) {
+        if (i % 2 == 0) {
+            printf("%d ", i);
+        }
+    }
     printf("\n");
+
     return 0;
 }
